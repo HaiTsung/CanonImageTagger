6620291f457d68fe10434e23821ffa734ad5929a
HaiTsung <126521692+HaiTsung@users.noreply.github.com>
2025-08-01 13:56:01 +0200

    Update CleanWorkspace.py

--- CleanWorkspace.py
+++ CleanWorkspace.py
@@ -1,13 +1,7 @@
 import os
-from os import remove
 import glob
 
-input = glob.glob("input/*.jpg")
 output = glob.glob("output/*.jpg")
-
-for image in input:
-    print(f"removed {image}")
-    os.remove(image)
 
 for image in output:
     print(f"removed {image}")
